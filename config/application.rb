01f5d1896d8de31d7c5949594bf9c3db7c17edf8
AllRom <allrom@ya.ru>
2019-01-22 19:52:33 +0300

    Lesson Rails Intro

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module TestGuru
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.time_zone = 'Moscow'
+    config.i18n.default_locale = 'en'
   end
 end
---
+++ config/initializers/version.rb
@@ -0,0 +1,5 @@
+# Application version info. According to https://semver.org/
+#
+class Application
+  VERSION = '0.1.0'
+end
