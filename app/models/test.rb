3bac1299699e19b87430804bfd76c007414a7fff
AllRom <allrom@ya.ru>
2019-02-04 17:14:59 +0300

    Lesson ActiveRecord_Refined#2

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@ class Test < ApplicationRecord
   has_and_belongs_to_many :users
   belongs_to :category
 
-  def self.pick_by_category(category_title)
-    Test.joins(:category).where("categories.title = ?", category_title).order('id DESC')
+  def self.pick_by_category(title)
+    Test.joins(:category).where("categories.title = ?", title).pluck(:title).sort.reverse
   end
 end
