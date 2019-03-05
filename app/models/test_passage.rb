dbe497ebf6e78a68df29a6c4ce2c9329ae8fe6ca
AllRom <allrom@ya.ru>
2019-03-06 01:09:14 +0300

    Lesson App_Logic#2

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,10 +1,14 @@
 class TestPassage < ApplicationRecord
+  PASS_MARK = 85.0
+
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: "Question", optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-  before_update     :before_update_set_next_question, unless: :completed?
+  ## before_validation :before_validation_set_first_question, on: :create
+  ## before_update     :before_update_set_next_question, unless: :completed?
+
+  before_save :before_save_set_next_question, on: %i[create, update]
 
   def completed?
     current_question.nil?
@@ -18,14 +22,32 @@ class TestPassage < ApplicationRecord
     save!
   end
 
-  private
-
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def result_percentage
+    correct = self.correct_questions
+    total = self.test.questions.count
+    (correct.to_f / total.to_f * 100.0).round(1)
   end
 
-  def before_update_set_next_question
-    self.current_question = next_question
+  def pass?
+    result_percentage >= PASS_MARK
+  end
+
+  def current_question_number
+    self.test.questions.pluck(:id).index(current_question.id) + 1
+  end
+
+  private
+
+  ## def before_validation_set_first_question
+  ##   self.current_question = test.questions.first if test.present?
+  ## end
+
+  ## def before_update_set_next_question
+  ##   self.current_question = next_question
+  ## end
+
+  def before_save_set_next_question
+    self.current_question = self.new_record? ? test.questions.first : next_question
   end
 
   def correct_answer?(answer_ids)
