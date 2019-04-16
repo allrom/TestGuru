be831fe7d411b373f83701619caa5e59f666d5d9
AllRom <allrom@ya.ru>
2019-04-16 13:04:20 +0300

    Lesson Rails_JS

---
+++ app/assets/javascripts/utilities/password_pair.js
@@ -0,0 +1,38 @@
+/*
+ * This file compares two password fields for matching.
+ */
+document.addEventListener('turbolinks:load', function() {
+  var controls = [];
+  var controlConf = document.querySelector('.confirmed-user-password');
+  var controlNew = document.querySelector('.created-user-password');
+
+  if (controlConf && controlNew) {
+    controls.push('controlConf', 'controlNew');
+    controls.forEach(function(control) {
+      addEventListener('input', comparePasswdFields);
+    });
+  }
+});
+
+function comparePasswdFields() {
+  // collect data
+  var passwordVal = document.getElementById("user_password").value;
+  var passwordConfirmedVal = document.getElementById("user_password_confirmation").value;
+
+  // call function to show red/green icon
+  if (passwordConfirmedVal) iconToggler(passwordVal, passwordConfirmedVal);
+}
+
+function iconToggler (val1, val2) {
+  var controlIcon = document.querySelector('.octicon-shield');
+  // console.log(controlIcon);
+
+  if (val1 !== val2) {
+    controlIcon.classList.remove('hide', 'match');
+    controlIcon.classList.add('mismatch');
+  }
+  else {
+    controlIcon.classList.remove('hide', 'mismatch');
+    controlIcon.classList.add('match');
+  }
+}
